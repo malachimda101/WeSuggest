f85070be023cfadadaff20a680f6303a65b1f58f
Stanley Markman <lowerlabstan@gmail.com>
2021-04-24 12:22:01 -0400

    basic routing

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 
 @app.route("/")
-def index():
-    return render_template('index.html', my_string="Wheeeee!", my_list=[0,1,2,3,4,5])
+def index(methods=['GET', 'POST']):
+    if request.method == 'GET':
+        return render_template('index.html', my_string="Wheeeee!", my_list=[0,1,2,3,4,5])
+    if request.method == 'POST':
+        print(request.form['complaint'])
+        return redirect(url_for('complaints'))
 
 @app.route("/complaints")
 def complaints():
-
-    return render_template('complaints.html', complaint="People in musichouse can't flush the toilet for shit.", upvotes=29)
+    return render_template('complaints.html', complaintid=49, complaint="People in musichouse can't flush the toilet for shit.", upvotes=29)
 
 if __name__ == '__main__':
     app.run(debug=True)
