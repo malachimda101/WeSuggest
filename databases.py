586bb120123b5125a701eeef1299e65c3bb9a516
jabarawad <jabar.awad@gmail.com>
2021-04-24 10:52:04 -0400

    Update databases.py
    Created submit function for database creation using connections

--- databases.py
+++ databases.py
@@ -1,3 +1,4 @@
+
 from functools import partial
 import sqlite3
 
@@ -9,3 +10,16 @@ class DatabaseStorage():
         self.email = "" 
         self.complaint = "" 
 
+    def submit(self): 
+        #connect to the database 
+        conn = sqlite3.connect("WesComplain_library_info.db")
+        c=conn.cursor 
+        c.execute("INSERT INTO complaints VALUES (:first, :last, :email, :complaint)", 
+            {'first':self.first_name, 
+             'last':self.last_name,
+             'email':self.email,
+             'complaint':self.complaint
+            }    
+        )
+        conn.commit() 
+        conn.close() 
