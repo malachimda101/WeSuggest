9f2ee946b2a2ed9a0f0662b041635925ba1df370
Stanley Markman <lowerlabstan@gmail.com>
2021-04-24 12:07:57 -0400

    added complaints viewer

--- app.py
+++ app.py
@@ -3,9 +3,13 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def template_test():
-    return render_template('template.html', my_string="Wheeeee!", my_list=[0,1,2,3,4,5])
+def index():
+    return render_template('index.html', my_string="Wheeeee!", my_list=[0,1,2,3,4,5])
 
+@app.route("/complaints")
+def complaints():
+
+    return render_template('complaints.html', complaint="People in musichouse can't flush the toilet for shit.", upvotes=29)
 
 if __name__ == '__main__':
     app.run(debug=True)
