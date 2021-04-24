b3ebc8e27180d1d6811732b95ac032634a5f1e60
Stanley Markman <lowerlabstan@gmail.com>
2021-04-24 12:32:54 -0400

    test

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 
-@app.route("/")
-def index(methods=['GET', 'POST']):
+@app.route("/", methods=['GET', 'POST'])
+def index():
     if request.method == 'GET':
         return render_template('index.html', my_string="Wheeeee!", my_list=[0,1,2,3,4,5])
     if request.method == 'POST':
