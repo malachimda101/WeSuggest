b93ca335e3d7568abf47f47f7ec0e9f586f6b089
jabarawad <jabar.awad@gmail.com>
2021-04-24 12:11:16 -0400

    Update databases.py
    added a query function to map entries to a dictionary with keys as ids and values; added values to init and submit

--- databases.py
+++ databases.py
@@ -9,17 +9,39 @@ class DatabaseStorage():
         self.last_name = "" 
         self.email = "" 
         self.complaint = "" 
+        self.id = ""
+        self.num_upvotes= ""
 
     def submit(self): 
         #connect to the database 
         conn = sqlite3.connect("WesComplain_library_info.db")
-        c=conn.cursor 
-        c.execute("INSERT INTO complaints VALUES (:first, :last, :email, :complaint)", 
-            {'first':self.first_name, 
+        c = conn.cursor 
+        c.execute("INSERT INTO complaints VALUES (:id, :first, :last, :email, :complaint, :upvotes)", 
+            {'id':self.id,
+             'first':self.first_name, 
              'last':self.last_name,
              'email':self.email,
-             'complaint':self.complaint
+             'complaint':self.complaint,
+             'upvotes':self.num_upvotes
             }    
         )
         conn.commit() 
         conn.close() 
+
+    def query(self):
+        conn = sqlite3.connect("WesComplain_library_info.db")
+        c = conn.cursor 
+        c.execute("SELECT * oid FROM complaints")
+        records = c.fetchall 
+
+        query_table = {} 
+        for record in records:
+            key = record[0]
+            value = (record[4], record[5])
+            query_table[key] = value 
+        
+        conn.commit()
+        conn.close()
+        
+        return query_table
+
